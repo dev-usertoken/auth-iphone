4770f84b09fb4d02493e8d9cc8a4ec7eee9505f6
hackathon <hack@Authorizeds-MacBook-Air-3.local>
2015-03-18 13:38:25 -0700

    first commit

---
+++ authentication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  authentication
+//
+//  Created by hackathon on 3/2/15.
+//  Copyright (c) 2015 hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ authentication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  authentication
+//
+//  Created by hackathon on 3/2/15.
+//  Copyright (c) 2015 hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
